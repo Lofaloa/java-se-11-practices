e52c57c1cf7609abced9e3ac144ae6936891e115
Logan Farci <farci.logan@gmail.com>
2021-04-09 10:56:43 +0200

    practice 4-1: initial commit to product manager

---
+++ product-manager/src/me/loganfarci/tutorials/shop/app/Shop.java
@@ -0,0 +1,11 @@
+package me.loganfarci.tutorials.shop.app;
+
+import me.loganfarci.tutorials.shop.data.Product;
+
+public class Shop {
+
+    public static void main(String[] args) {
+        Product product = new Product();
+        System.out.println();
+    }
+}
