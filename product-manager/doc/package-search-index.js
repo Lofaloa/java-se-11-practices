7aa1c32d6f39a01ba6d012bf29ba64dd51d4ad16
Logan Farci <farci.logan@gmail.com>
2021-04-17 10:13:50 +0200

    Only the product manager source files are tracked

--- product-manager/doc/package-search-index.js
+++
@@ -1,1 +0,0 @@
-packageSearchIndex = [{"l":"All Packages","url":"allpackages-index.html"},{"l":"me.loganfarci.tutorials.shop.app"},{"l":"me.loganfarci.tutorials.shop.data"}]
--- product-manager/doc/type-search-index.js
+++
@@ -1,1 +0,0 @@
-typeSearchIndex = [{"l":"All Classes","url":"allclasses-index.html"},{"p":"me.loganfarci.tutorials.shop.data","l":"Product"},{"p":"me.loganfarci.tutorials.shop.app","l":"Shop"}]
